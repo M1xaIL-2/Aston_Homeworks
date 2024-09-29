8e7fc0019ad625884e68d2f0974a97ae04d85c9e
M1xaIL-2 <145340275+M1xaIL-2@users.noreply.github.com>
2024-09-29 16:47:12 +0400

    Практическое задание Л10
    Исключения

--- Lesson_10/src/com/company/Array.java
+++ Lesson_10/src/com/company/Array.java
@@ -1,23 +1,16 @@
 package com.company;
 
 public class Array {
-    static class MyArraySizeException extends Exception {
-        public MyArraySizeException(String message) {
-            super(message); //передача сообщения родительскому классу Exception
-        }
-    }
-
-    static class MyArrayDataException extends Exception {
-        public MyArrayDataException(String message) {
-            super(message);
-        }
-    }
-
-    //метод "пробрасывания" исключения MyArraySizeException и MyArrayDataException
+       //метод "пробрасывания" исключения MyArraySizeException и MyArrayDataException
     public static int elementsSum(String[][] Arr) throws MyArraySizeException, MyArrayDataException {
         int sum = 0;
-        if (Arr.length != 4 || Arr[0].length != 4) {
-            throw new MyArraySizeException("Размер массива несовпадает с размеров 4х4");
+        if (Arr.length != 4){
+            throw new MyArraySizeException("Количество строк должно быть 4");
+        }
+        for (String [] stolb : Arr) {
+            if (stolb.length!=4) {
+                throw new MyArraySizeException("Количество столбцов должно быть 4");
+            }
         }
 
         for (int i = 0; i < Arr.length; i++) {
